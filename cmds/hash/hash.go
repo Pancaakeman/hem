476495a3c013da665e0e7d5735e106dc8837b080
Pancaakeman <haxionrising@gmail.com>
2025-11-13 01:08:17 +0000

    added functionality to the txthash cmd

--- cmds/hash/hash.go
+++ cmds/hash/hash.go
@@ -2,6 +2,7 @@ package hash
 
 import (
 	"context"
+	"crypto/sha256"
 	"log"
 
 	"github.com/urfave/cli/v3"
@@ -23,23 +24,27 @@ func TextHash() *cli.Command {
 	txthash := cli.Command{
 		Name:        "txtHash",
 		Usage:       "Outputs a hash digest for the inputted text",
+		UsageText:   "txthash --sha256",
 		Description: "Outputs a hash digest for the input text, Uses SHA3-256 by default unless otherwise is specified in the flag",
+		Aliases:     []string{"th", "txthash", "texthash", "TxtHash"},
+
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:  "sha-256",
+				Name:  "alg",
 				Value: "SHA-256",
 				Usage: "Hashing Algorithm to use for the hash",
 			},
 		},
 		Action: func(ctx context.Context, command *cli.Command) error {
-			if command.String("hash_alg") == "sha-256" {
-				log.Println("sha-256")
+			if command.String("alg") == "sha256" {
+				hasher := sha256.New()
+				hasher.Write([]byte(command.Args().Get(0)))
+				log.Printf("%x", hasher.Sum(nil))
 			} else {
 				log.Println("bad :(")
 			}
 			return nil
 		},
 	}
-	log.Println(txthash)
 	return &txthash
 }
